018b95500451992d3be0b48a6a928ea8e5f6a13c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-09-20 12:42:13 +0200

    Introduce services HttpClient injection as part of the HttpFactory pattern. Install required libraries

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Singleton
+﻿using DesignPatterns;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Singleton
 {
-    public static class Program
+    public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ Singleton/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesignPatterns
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
