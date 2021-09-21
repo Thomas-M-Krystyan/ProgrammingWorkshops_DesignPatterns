e136cd62cd9873f7bc7e426f0f77f0d6b1eb799d
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-09-21 03:27:45 +0200

    Include Singleton cases in unit tests

--- SingletonTests/BusinessLogicTests.cs
+++ SingletonTests/BusinessLogicTests.cs
@@ -5,6 +5,7 @@ using Singleton.Exercise.Implementations;
 using Singleton.Exercise.Implementations.Abstractions;
 using Singleton.Exercise.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -16,23 +17,29 @@ namespace SingletonTests
     {
         private readonly HashSet<int> _clientIds = new();
 
-        [TestCaseSource(nameof(HttpClientHandlers))]
+        [TestCaseSource(nameof(HttpClientHandlers_Factory))]
+        [TestCaseSource(nameof(HttpClientHandlers_Singleton))]
         public async Task CheckIfMethod_GetResponse_ReturnsOkStatusCode(IHttpClientHandler httpClientHandler)
         {
             Assert.That(await httpClientHandler.GetResponse("http://webcode.me"), Is.EqualTo(HttpStatusCode.OK));
             TestContext.WriteLine(httpClientHandler.GetImplementationName());
         }
-        
-        [TestCaseSource(nameof(HttpClientHandlers))]
+
+        // NOTE: When checking unique client ID the expected result can be achieved only if mocked HttpClientFactory is set up once
+        [TestCaseSource(nameof(HttpClientHandlers_Factory))]
+        // NOTE: When checking unique client ID we are always using the same HttpClient. So, does not matter how many times the implementation will be called
+        [TestCaseSource(nameof(HttpClientHandlers_Singleton))]
         public void CheckIfMethod_GetClientId_ReturnsTheSameId(IHttpClientHandler httpClientHandler)
         {
-            this._clientIds.Add(httpClientHandler.GetClientId());
-            
+            var clientId = httpClientHandler.GetClientId();
+
+            this._clientIds.Add(clientId);
+
             Assert.That(this._clientIds.Count, Is.EqualTo(1), message: "Expected is to have only a single unique client ID");
-            TestContext.WriteLine(httpClientHandler.GetImplementationName());
+            TestContext.WriteLine($"{httpClientHandler.GetImplementationName()}: {clientId}");
         }
 
-        private static IHttpClientHandler[] HttpClientHandlers()
+        private static IHttpClientHandler[] HttpClientHandlers_Factory()
         {
             var logger = new NullLogger<BaseHttpClientHandler>();
 
@@ -48,6 +55,26 @@ namespace SingletonTests
                 new BusinessLogic2(logger, httpClientFactory),
                 new BusinessLogic3(logger, httpClientFactory),
                 new BusinessLogic4(logger, httpClientFactory)
+            };
+        }
+
+        private static IHttpClientHandler[] HttpClientHandlers_Singleton()
+        {
+            var logger = new NullLogger<BaseHttpClientHandler>();
+
+            return new IHttpClientHandler[]
+            {
+                new BusinessLogic1(logger),
+                new BusinessLogic1(logger),
+                new BusinessLogic1(logger),
+
+                new BusinessLogic2(logger),
+                new BusinessLogic2(logger),
+
+                new BusinessLogic3(logger),
+
+                new BusinessLogic4(logger),
+                new BusinessLogic4(logger)
             };
         }
     }
