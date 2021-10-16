8cd08ed455c64f04a49bf17084792fa6ab9b2606
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-16 19:11:05 +0200

    Add Node class - the graph building block

---
+++ Strategy/Node.cs
@@ -0,0 +1,32 @@
+﻿namespace Strategy
+{
+    /// <summary>
+    /// The building element of <see cref="Graph"/>.
+    /// </summary>
+    public sealed class Node
+    {
+        /// <summary>
+        /// Gets the <see cref="Node"/>'s value.
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// Gets or sets the next (nested) left <see cref="Node"/>.
+        /// </summary>
+        public Node NextLeft { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next (nested) right <see cref="Node"/>.
+        /// </summary>
+        public Node NextRight { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
+        /// <param name="value">The value of the <see cref="Node"/>.</param>
+        public Node(string value)
+        {
+            this.Value = value;
+        }
+    }
+}
