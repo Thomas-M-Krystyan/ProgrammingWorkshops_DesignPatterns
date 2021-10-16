09356516363c971419c550d17f57a25ba44d4ba4
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-16 19:09:45 +0200

    Add test project for Strategy

---
+++ StrategyTests/DataTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace StrategyTests
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        public void GraphContains10Nodes()
+        {
+            Assert.Fail();
+        }
+    }
+}
