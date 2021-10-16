56834cff161c11470277a9caaf96a01bdb289224
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-17 01:25:52 +0200

    Add user feedback layer in Program

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace Strategy
+﻿using System;
+
+namespace Strategy
 {
     public static class Program
     {
         public static void Main()
         {
+            var value = AskForInput();
+            
+            switch (value)
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+
+                case "1":
+                    // 1. Use BFS algorithm
+                    // 2. Print result
+                    break;
+
+                case "2":
+                    // 1. Use DFS algorithm
+                    // 2. Print result
+                    break;
+
+                default:
+                    Console.WriteLine("This command is not supported.\n");
+                    break;
+            }
+
+            Main();
+        }
+
+        private static string AskForInput()
+        {
+            Console.WriteLine(
+                "Choose an algorithm:" +
+                "\n[0] to EXIT" +
+                "\n[1] BFS" +
+                "\n[2] DFS" +
+                "\nand press ENTER");
+
+            return Console.ReadLine();
         }
     }
 }
