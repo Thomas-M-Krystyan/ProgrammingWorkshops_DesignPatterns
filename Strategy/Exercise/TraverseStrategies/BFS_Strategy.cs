9eb98072e3b520a5264022288b4664675db3b56c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-17 02:34:30 +0200

    Add public methods in both algorithms

--- Strategy/Exercise/TraverseStrategies/BFS_Strategy.cs
+++ Strategy/Exercise/TraverseStrategies/BFS_Strategy.cs
@@ -1,9 +1,19 @@
-﻿namespace Strategy.Exercise.TraverseStrategies
+﻿using Strategy.Exercise.Result;
+using System;
+
+namespace Strategy.Exercise.TraverseStrategies
 {
     /// <summary>
     /// Breadth First Search (BFS) algorithm.
     /// </summary>
     public sealed class BFS_Strategy
     {
+        /// <summary>
+        /// Try to find the given element.
+        /// </summary>
+        public TraverseResult Find(string value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Strategy/Exercise/TraverseStrategies/DFS_Strategy.cs
+++ Strategy/Exercise/TraverseStrategies/DFS_Strategy.cs
@@ -1,9 +1,19 @@
-﻿namespace Strategy.Exercise.TraverseStrategies
+﻿using Strategy.Exercise.Result;
+using System;
+
+namespace Strategy.Exercise.TraverseStrategies
 {
     /// <summary>
     /// Depth First Search (DFS) algorithm.
     /// </summary>
     public sealed class DFS_Strategy
     {
+        /// <summary>
+        /// Try to find the given element.
+        /// </summary>
+        public TraverseResult Find(string value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
