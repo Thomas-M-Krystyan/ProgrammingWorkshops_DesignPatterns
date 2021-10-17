e2a431b91eae5f9888a7872f9f7891ede81af399
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-17 03:06:11 +0200

    Add unit tests for strategies

--- StrategyTests/BFS_StrategyTests.cs
+++ StrategyTests/BFS_StrategyTests.cs
@@ -1,9 +1,60 @@
 ﻿using NUnit.Framework;
+using Strategy.Exercise.TraverseStrategies;
+using System;
 
 namespace StrategyTests
 {
     [TestFixture]
     public class BFS_StrategyTests
     {
+        private BFS_Strategy _strategy;
+
+        [SetUp]
+        public void InitializeTests()
+        {
+            this._strategy = new();
+        }
+
+        [Test]
+        public void FindElementA()
+        {
+            // Act
+            var result = this._strategy.Find("A");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.IsFound);
+            Assert.That(result.Path, Is.EqualTo("A"));
+            Assert.That(result.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FindElementE()
+        {
+            // Act
+            var result = this._strategy.Find("E");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.IsFound);
+            Assert.That(result.Path, Is.EqualTo("ABCDE"));
+            Assert.That(result.Count, Is.EqualTo(5));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("Z")]
+        public void FindElement_ForNotExistingValue_ReturnsEmptyResult(string value)
+        {
+            // Act
+            var result = this._strategy.Find(value);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.IsFound);
+            Assert.That(result.Path, Is.EqualTo(String.Empty));
+            Assert.That(result.Count, Is.EqualTo(0));
+        }
     }
 }
