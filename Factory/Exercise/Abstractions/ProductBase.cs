aa97deea8d7d65390082af951fc8958f653c47e7
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-24 15:06:06 +0200

    Prepare base abstract product type

---
+++ Factory/Exercise/Abstractions/ProductBase.cs
@@ -0,0 +1,69 @@
+﻿using Factory.Exercise.Interfaces;
+using System;
+
+namespace Factory.Exercise.Abstractions
+{
+    /// <summary>
+    /// The base class for all products.
+    /// </summary>
+    /// <seealso cref="IProduct" />
+    public abstract class ProductBase : IProduct
+    {
+        /// <summary>
+        /// Gets the product type.
+        /// </summary>
+        public Enum Type { get; }
+
+        /// <summary>
+        /// Gets the product weight (in kilograms).
+        /// </summary>
+        public double WeightKg { get; }
+
+        /// <summary>
+        /// Gets the product price (in euro).
+        /// </summary>
+        public decimal PriceEur { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductBase"/> class.
+        /// </summary>
+        /// <param name="type">The product type.</param>
+        /// <param name="weight">The product weight in KG.</param>
+        /// <param name="price">The product price in EUR.</param>
+        protected ProductBase(Enum type, double weightKg, decimal priceEur)
+        {
+            this.Type = type;
+            this.WeightKg = weightKg;
+            this.PriceEur = priceEur;
+        }
+
+        /// <inheritdoc />
+        public new string GetType()
+        {
+            return this.Type.ToString();  // NOTE: Get enum name
+        }
+
+        /// <inheritdoc />
+        public abstract string GetName();
+
+        /// <inheritdoc />
+        public string GetWeightInKg()
+        {
+            return $"{this.WeightKg} kg";
+        }
+
+        /// <inheritdoc />
+        public string GetWeightInLb()
+        {
+            const double KilogramsToPundsFactor = 2.20462262;
+
+            return $"{this.WeightKg * KilogramsToPundsFactor} lb";
+        }
+
+        /// <inheritdoc />
+        public string GetPriceInEur()
+        {
+            return $"€ {this.PriceEur}";
+        }
+    }
+}
