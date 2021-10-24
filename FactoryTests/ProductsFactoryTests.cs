34123c10c0fc4f96beab5f5a70f0475b66a92394
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-24 22:13:29 +0200

    Update unit tests

--- FactoryTests/ProductsFactoryTests.cs
+++ FactoryTests/ProductsFactoryTests.cs
@@ -1,25 +1,41 @@
-using Factory.Exercise.Enums;
+using Factory.Exercise.Abstractions;
 using Factory.Exercise.Factories;
 using Factory.Exercise.Models;
 using NUnit.Framework;
+using System;
+using System.Reflection;
 
 namespace FactoryTests
 {
     [TestFixture]
     public class ProductsFactoryTests
     {
-        [TestCase]
-        public void Check_Method_Get_ReturnsExpectedModelType()
+        private ProductsFactory _factory;
+
+        [OneTimeSetUp]
+        public void InitializeTests()
         {
+            this._factory = new ProductsFactory();
+        }
+
+        [TestCase(typeof(WholeGrainBread), "Whole Grain Bread", "0,75 kg", "€ 1.65")]
+        [TestCase(typeof(ToastBread), "Toast Bread", "1 kg", "€ 1.20")]
+        public void Check_Method_Get_ForWholeGrainBread_ReturnsExpectedModelType(Type type, string name, string weight, string price)
+        {
+            // NOTE: The reflection mechanism is used to invoke _factory.Get<T> method with parameter-based types in test cases
+
             // Arrange
-            var factory = new ProductsFactory();
+            var factoryGetMethod = this._factory.GetType().GetMethod(nameof(this._factory.Get), BindingFlags.Instance | BindingFlags.Public);
+            factoryGetMethod = factoryGetMethod.MakeGenericMethod(type);
 
             // Act
-            var result = factory.Get<Bread>(BreadTypes.Toast);
+            var result = (ProductBase)factoryGetMethod.Invoke(this._factory, null);
 
             // Assert
-            Assert.That(result.GetType(), Is.EqualTo(typeof(Bread)));
-            Assert.That(result.GetTypeName(), Is.EqualTo("Toast"));
+            Assert.That(result.GetType(), Is.EqualTo(type));
+            Assert.That(result.GetName(), Is.EqualTo(name));
+            Assert.That(result.GetWeightInKg, Is.EqualTo(weight));
+            Assert.That(result.GetPriceInEur, Is.EqualTo(price));
         }
     }
 }
--- FactoryTests/ProductsTests.cs
+++ FactoryTests/ProductsTests.cs
@@ -1,7 +1,8 @@
-﻿using Factory.Exercise.Enums;
-using Factory.Exercise.Models;
+﻿using Factory.Exercise.Abstractions;
+using Moq;
 using NUnit.Framework;
 using System;
+using System.Reflection;
 using System.Text.Json;
 
 namespace FactoryTests
@@ -9,49 +10,48 @@ namespace FactoryTests
     [TestFixture]
     public class ProductsTests
     {
-        private const BreadTypes ValidBreadType = BreadTypes.Toast;
         private const double ValidWeightInKg = 1;
         private const decimal ValidPriceInEur = 1.20M;
 
         [Test]
-        public void Model_Bread_ForValidInput_ReturnsExpectedValues()
+        public void Model_Base_ForValidInput_ReturnsExpectedValues()
         {
             // Act
-            var model = new Bread(ValidBreadType, ValidWeightInKg, ValidPriceInEur);
+            var model = new Mock<ProductBase>(ValidWeightInKg, ValidPriceInEur).Object;
 
             // Assert
             var actualSerializedModelData = JsonSerializer.Serialize(model);
-            var expectedSerializedModelData = "{\"Type\":{},\"WeightKg\":1,\"PriceEur\":1.20}";
+            var expectedSerializedModelData = "{\"WeightKg\":1,\"PriceEur\":1.20}";
 
             Assert.That(actualSerializedModelData, Is.EqualTo(expectedSerializedModelData));
 
-            Assert.That(model.GetTypeName(), Is.EqualTo("Toast"));
-            Assert.That(model.GetName(), Is.EqualTo("Toast Bread"));
             Assert.That(model.GetWeightInKg(), Is.EqualTo("1 kg"));
             Assert.That(model.GetWeightInLb(), Is.EqualTo("2.20 lb"));
             Assert.That(model.GetPriceInEur(), Is.EqualTo("€ 1.20"));
         }
 
-        [TestCase((BreadTypes)(-1))]
-        [TestCase((BreadTypes)9999)]
-        public void Model_Bread_ForInvalidType_ReturnExceptions(BreadTypes invalidType)
+        [Test]
+        public void Model_Base_ForInvalidWeight_ReturnExceptions()
         {
             // Act & Assert
-            Assert.Throws<ArgumentException>(() => new Bread(invalidType, ValidWeightInKg, ValidPriceInEur));
+            var baseException = Assert.Throws<TargetInvocationException>(() =>
+            {
+                _ = new Mock<ProductBase>(-1, ValidPriceInEur).Object;
+            });
+
+            Assert.That(baseException.InnerException.GetType, Is.EqualTo(typeof(ArgumentException)));
         }
 
         [Test]
-        public void Model_Bread_ForInvalidWeight_ReturnExceptions()
+        public void Model_Base_ForInvalidPrice_ReturnExceptions()
         {
             // Act & Assert
-            Assert.Throws<ArgumentException>(() => new Bread(ValidBreadType, -1, ValidPriceInEur));
-        }
+            var baseException = Assert.Throws<TargetInvocationException>(() =>
+            {
+                _ = new Mock<ProductBase>(ValidWeightInKg, -1M).Object;
+            });
 
-        [Test]
-        public void Model_Bread_ForInvalidPrice_ReturnExceptions()
-        {
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => new Bread(ValidBreadType, ValidWeightInKg, -1));
+            Assert.That(baseException.InnerException.GetType, Is.EqualTo(typeof(ArgumentException)));
         }
     }
 }
