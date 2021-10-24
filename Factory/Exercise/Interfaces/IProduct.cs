06c8a2fa6e2bfbe2c823172ad9e53e2463d77714
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-24 15:06:32 +0200

    Prepare interface for products

---
+++ Factory/Exercise/Interfaces/IProduct.cs
@@ -0,0 +1,34 @@
+﻿namespace Factory.Exercise.Interfaces
+{
+    /// <summary>
+    /// The interface for all products.
+    /// </summary>
+    public interface IProduct
+    {
+        /// <summary>
+        /// Gets the product type.
+        /// </summary>
+        string GetType();
+
+        /// <summary>
+        /// Gets the product name.
+        /// </summary>
+        string GetName();
+
+        /// <summary>
+        /// Gets the product weight (in kilograms).
+        /// </summary>
+        /// <returns></returns>
+        string GetWeightInKg();
+
+        /// <summary>
+        /// Gets the product weight (in pounds).
+        /// </summary>
+        string GetWeightInLb();
+
+        /// <summary>
+        /// Gets the product price (in euro).
+        /// </summary>
+        string GetPriceInEur();
+    }
+}
