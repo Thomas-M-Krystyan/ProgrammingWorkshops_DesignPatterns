7aaef1fbd4829825002a571394f3566cfc0900cb
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-10-24 19:08:26 +0200

    Add unit tests for Factory method to get Bread product

--- FactoryTests/ProductsFactoryTests.cs
+++ FactoryTests/ProductsFactoryTests.cs
@@ -1,3 +1,6 @@
+using Factory.Exercise.Enums;
+using Factory.Exercise.Factories;
+using Factory.Exercise.Models;
 using NUnit.Framework;
 
 namespace FactoryTests
@@ -5,5 +8,18 @@ namespace FactoryTests
     [TestFixture]
     public class ProductsFactoryTests
     {
+        [TestCase]
+        public void Check_Method_Get_ReturnsExpectedModelType()
+        {
+            // Arrange
+            var factory = new ProductsFactory();
+
+            // Act
+            var result = factory.Get<Bread>(BreadTypes.Toast);
+
+            // Assert
+            Assert.That(result.GetType(), Is.EqualTo(typeof(Bread)));
+            Assert.That(result.GetTypeName(), Is.EqualTo("Toast"));
+        }
     }
 }
