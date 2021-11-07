74af20c38d45f2c2f09dc0f23c0aed50ac349844
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-11-07 11:28:23 +0100

    Implement BFS algorithm

--- Strategy/Exercise/TraverseStrategies/BFS_Strategy.cs
+++ Strategy/Exercise/TraverseStrategies/BFS_Strategy.cs
@@ -1,5 +1,8 @@
-﻿using Strategy.Exercise.Result;
+﻿using Strategy.Exercise.Data;
+using Strategy.Exercise.Result;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Strategy.Exercise.TraverseStrategies
 {
@@ -13,7 +16,46 @@ namespace Strategy.Exercise.TraverseStrategies
         /// </summary>
         public TraverseResult Find(string value)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                return default;
+            }
+
+            // Initialize queue
+            var queue = new Queue<Node>(Graph.Count);  // NOTE: Queue can have fixed size, which reduce resize operations on internal array
+
+            queue.Enqueue(Graph.StartNode);  // NOTE: First element of the queue is always the first Node of the Graph
+
+            // Results
+            var path = new StringBuilder();
+            ushort count = 0;
+
+            // Start traversing
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                path.Append(currentNode.Value);
+                count++;
+
+                if (currentNode.Value == value)  // The searched Node was found
+                {
+                    return new TraverseResult(true, path.ToString(), count);
+                }
+
+                // Adding left and right Nodes to the queue
+                AddTo(queue, currentNode.NextLeft);
+                AddTo(queue, currentNode.NextRight);
+            }
+            
+            return default;  // Nothing was found
+        }
+
+        private static void AddTo(Queue<Node> queue, Node node)
+        {
+            if (node != null)
+            {
+                queue.Enqueue(node);
+            }
         }
     }
 }
