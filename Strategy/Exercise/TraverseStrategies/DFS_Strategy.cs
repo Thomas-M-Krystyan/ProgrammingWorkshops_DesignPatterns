e948430105d90ab26f408466e78f5bcb1691acb5
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-11-07 12:07:11 +0100

    Add implementation of DFS algorithm

--- Strategy/Exercise/TraverseStrategies/DFS_Strategy.cs
+++ Strategy/Exercise/TraverseStrategies/DFS_Strategy.cs
@@ -1,5 +1,8 @@
-﻿using Strategy.Exercise.Result;
+﻿using Strategy.Exercise.Data;
+using Strategy.Exercise.Result;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Strategy.Exercise.TraverseStrategies
 {
@@ -8,12 +11,78 @@ namespace Strategy.Exercise.TraverseStrategies
     /// </summary>
     public sealed class DFS_Strategy
     {
+        private readonly bool _isLeftHanded;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DFS_Strategy"/> class.
+        /// </summary>
+        /// <param name="isLeftHanded">
+        ///   If <c>true</c> left-handed version of DFS algorithm will be used;
+        ///   otherwise, if <c>false</c>, right-handed version will be used.
+        /// </param>
+        public DFS_Strategy(bool isLeftHanded)
+        {
+            this._isLeftHanded = isLeftHanded;
+        }
+
         /// <summary>
         /// Try to find the given element.
         /// </summary>
         public TraverseResult Find(string value)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                return default;
+            }
+
+            // Initialize stack
+            var stack = new Stack<Node>(Graph.Count);  // NOTE: Stack can have fixed size, which reduce resize operations on internal array
+
+            stack.Push(Graph.StartNode);  // NOTE: First element of the stack is always the first Node of the Graph
+
+            // Results
+            var path = new StringBuilder();
+            ushort count = 0;
+
+            // Start traversing
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+                path.Append(currentNode.Value);
+                count++;
+
+                if (currentNode.Value == value)  // The searched Node was found
+                {
+                    return new TraverseResult(true, path.ToString(), count);
+                }
+
+                // Adding left and right Nodes to the stack
+                FollowTheRuleOfHand(stack, currentNode);
+            }
+
+            return default;  // Nothing was found
+        }
+
+        private void FollowTheRuleOfHand(Stack<Node> stack, Node currentNode)
+        {
+            if (this._isLeftHanded)
+            {
+                AddTo(stack, currentNode.NextLeft);
+                AddTo(stack, currentNode.NextRight);
+            }
+            else
+            {
+                AddTo(stack, currentNode.NextRight);
+                AddTo(stack, currentNode.NextLeft);
+            }
+        }
+
+        private static void AddTo(Stack<Node> stack, Node node)
+        {
+            if (node != null)
+            {
+                stack.Push(node);
+            }
         }
     }
 }
