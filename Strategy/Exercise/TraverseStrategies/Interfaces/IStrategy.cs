8d0e4ed51926868f57a8f09e262e2ef31a168e97
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-11-07 12:37:12 +0100

    Add strategy interface

---
+++ Strategy/Exercise/TraverseStrategies/Interfaces/IStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Strategy.Exercise.Result;
+
+namespace Strategy.Exercise.TraverseStrategies.Interfaces
+{
+    public interface IStrategy
+    {
+        /// <summary>
+        /// Try to find the given element.
+        /// </summary>
+        /// <param name="value">The value of the Node to be found.</param>
+        /// <param name="isLeftHanded">
+        ///   If <c>true</c> left-handed version of graph traversing algorithm will be used;
+        ///   otherwise, if <c>false</c>, right-handed version will be used.
+        /// </param>
+        TraverseResult Find(string value, bool isLeftHanded);
+    }
+}
