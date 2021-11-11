9a155fb08ef88137e8d1dc77de9519c60bbc70ca
Krystyan, Tomasz M <Tomasz.Krystyan@bakerhughes.com>
2021-11-11 13:55:19 +0100

    More corner cases for Adding service

--- FacadeTests/Services/AddingServiceTests.cs
+++ FacadeTests/Services/AddingServiceTests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 namespace FacadeTests.Services
 {
     [TestFixture]
-    public class Tests
+    public class AddingServiceTests
     {
         private AddingService _service;
 
@@ -15,54 +15,57 @@ namespace FacadeTests.Services
             this._service = new AddingService(new NullLogger<AddingService>());
         }
 
-        [Test]
-        public void Method_Calculate_ForIntNumbers_ReturnsExpectedSum()
+        [TestCase(new[] { 1, 2, 3, 4 }, 10)]
+        [TestCase(new[] { 0, 0, 0, 0 }, 0)]
+        [TestCase(new[] { 0, -0, -0, 0 }, 0)]
+        [TestCase(new[] { -1, -2, -3, -4, 0 }, -10)]
+        public void Method_Calculate_ForIntNumbers_ReturnsExpectedSum(int[] numbers, int expectedResult)
         {
             // Act
-            var result = this._service.Calculate(1, 2, 3, 4);
+            var actualResult = this._service.Calculate(numbers);
 
             // Assert
-            Assert.That(result, Is.EqualTo(10));
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
         }
 
         [Test]
         public void Method_Calculate_ForFloatNumbers_ReturnsExpectedSum()
         {
             // Act
-            var result = this._service.Calculate(0.5f, 2.5f, 3.1f, 3.9f);
+            var actualResult = this._service.Calculate(0.5f, 2.5f, 3.1f, 3.9f);
 
             // Assert
-            Assert.That(result, Is.EqualTo(10.0f));
+            Assert.That(actualResult, Is.EqualTo(10.0f));
         }
 
         [Test]
         public void Method_Calculate_ForDoubleNumbers_ReturnsExpectedSum()
         {
             // Act
-            var result = this._service.Calculate(0.75D, 2.25D, 2.91D, 4.09D);
+            var actualResult = this._service.Calculate(0.75D, 2.25D, 2.91D, 4.09D);
 
             // Assert
-            Assert.That(result, Is.EqualTo(10.0D));
+            Assert.That(actualResult, Is.EqualTo(10.0D));
         }
 
         [Test]
         public void Method_Calculate_ForDecimalNumbers_ReturnsExpectedSum()
         {
             // Act
-            var result = this._service.Calculate(0.75M, 2.25M, 2.91M, 4.09M);
+            var actualResult = this._service.Calculate(0.75M, 2.25M, 2.91M, 4.09M);
 
             // Assert
-            Assert.That(result, Is.EqualTo(10.0M));
+            Assert.That(actualResult, Is.EqualTo(10.0M));
         }
 
         [Test]
         public void Method_Calculate_ForObjects_ReturnsDefault_DoesNotThrowException()
         {
             // Act
-            var result = this._service.Calculate(new object(), null, null);
+            var actualResult = this._service.Calculate(new object(), null, null);
 
             // Assert
-            Assert.That(result, Is.Null);
+            Assert.That(actualResult, Is.Null);
         }
     }
 }
