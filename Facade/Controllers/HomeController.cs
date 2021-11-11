818a49eed97141133b280ffc938b46fc44e98c3f
Krystyan, Tomasz M <Tomasz.Krystyan@bakerhughes.com>
2021-11-11 14:19:08 +0100

    Add summaries for HomeController.cs

--- Facade/Controllers/HomeController.cs
+++ Facade/Controllers/HomeController.cs
@@ -2,8 +2,15 @@
 
 namespace Facade.Controllers
 {
+    /// <summary>
+    /// The main MVC Controller to display the Home page.
+    /// </summary>
+    /// <seealso cref="Controller" />
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Renders the first "Index" page.
+        /// </summary>
         public IActionResult Index()
         {
             // TODO: I want to have the result of calculation to be displayed in this view (Index.cshtml)
