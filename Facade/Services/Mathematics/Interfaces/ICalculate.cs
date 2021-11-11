13ed8082e774af9641f7d711f920f2d46a00a8b8
Krystyan, Tomasz M <Tomasz.Krystyan@bakerhughes.com>
2021-11-11 13:33:13 +0100

    Add generic calculation interface

---
+++ Facade/Services/Mathematics/Interfaces/ICalculate.cs
@@ -0,0 +1,16 @@
+﻿namespace Facade.Services.Mathematics.Interfaces
+{
+    /// <summary>
+    /// The interface for mathematics services.
+    /// </summary>
+    public interface ICalculate
+    {
+        /// <summary>
+        /// Calculates the specified numbers.
+        /// </summary>
+        /// <typeparam name="T">The type of number.</typeparam>
+        /// <param name="numbers">The numbers to be calculated.</param>
+        /// <returns>The result of calculation.</returns>
+        T Calculate<T>(params T[] numbers);
+    }
+}
