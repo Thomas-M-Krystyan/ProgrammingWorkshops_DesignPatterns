d83368988f3128e6c248a2edf4904ab9c70942e6
Krystyan, Tomasz M <Tomasz.Krystyan@bakerhughes.com>
2021-11-11 14:14:10 +0100

    Add simple display service

---
+++ Facade/Services/Displays/RichTextService.cs
@@ -0,0 +1,17 @@
+﻿using Facade.Services.Displays.Interfaces;
+
+namespace Facade.Services.Displays
+{
+    /// <summary>
+    /// Formats the original calculation result for display purposes.
+    /// </summary>
+    /// <seealso cref="IDisplay" />
+    public class RichTextService : IDisplay
+    {
+        /// <inheritdoc />
+        public string Enrich(object value)
+        {
+            return @$"The result is: {value}";
+        }
+    }
+}
