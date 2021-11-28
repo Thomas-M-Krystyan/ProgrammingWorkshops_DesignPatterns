baecfb9542a89027908cf64f1be7e82b06d2bddd
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-11-28 23:20:01 +0100

    Add this keyword in Startup

--- Facade/Startup.cs
+++ Facade/Startup.cs
@@ -15,7 +15,7 @@ namespace Facade
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
