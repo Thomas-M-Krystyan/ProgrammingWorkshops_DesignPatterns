a1c479fccbe02cf3c094674295f7ccab08216f0b
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-11-28 20:13:21 +0100

    Add DTO to pass numbers to calculate

---
+++ Facade/DTOs/CalculationDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Facade.DTOs
+{
+    /// <summary>
+    /// The DTO to pass numbers to calculate.
+    /// </summary>
+    /// <typeparam name="TAdd">The type of numbers to add.</typeparam>
+    /// <typeparam name="TMultiply">The type of numbers to multiply.</typeparam>
+    public sealed class CalculationDto<TAdd, TMultiply>
+    {
+        /// <summary>
+        /// Gets or sets the numbers to add.
+        /// </summary>
+        public TAdd[] NumbersToAdd { get; set; }
+
+        /// <summary>
+        /// Gets or sets the numbers to multiply.
+        /// </summary>
+        public TMultiply[] NumbersToMultiply { get; set; }
+    }
+}
