2cf3449954563f3e8b28304675001ae7b43702dd
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-12-10 19:32:57 +0100

    Configure logger

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Facade
 {
@@ -12,6 +13,11 @@ namespace Facade
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
