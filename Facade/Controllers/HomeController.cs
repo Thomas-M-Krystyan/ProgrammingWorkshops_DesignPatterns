721fcb0d93f43c1481e78e1e3259b97d95c626f9
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-12-10 20:14:49 +0100

    Fill round up flag for debug DTO in HomeController

--- Facade/Controllers/HomeController.cs
+++ Facade/Controllers/HomeController.cs
@@ -27,7 +27,8 @@ namespace Facade.Controllers
             dto = new CalculationDto
             {
                 NumbersToAdd = new[] { 4D, 8, 15, 16, 23, 42 },
-                NumbersToMultiply = new[] { 6, 3.12037037037037 }
+                NumbersToMultiply = new[] { 6, 3.12037037037037 },
+                UseRoundUp = true
             };
 
             var viewModel = new ResultViewModel
