5566162b444d79cc70c72efdf08c0223aa23c815
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2021-12-11 03:33:57 +0100

    Update display unit tests

--- FacadeTests/Services/RichTextServiceTests.cs
+++ FacadeTests/Services/RichTextServiceTests.cs
@@ -6,19 +6,36 @@ namespace FacadeTests.Services
     [TestFixture]
     public class RichTextServiceTests
     {
-        private const string ResultText = "Happy ";
+        private const string NewYearResult = "Happy ";
+        private const string StandardResult = "The result is: ";
 
-        [TestCase(null, ResultText + "!")]  // Value is empty
-        [TestCase("", ResultText + "!")]    // Value is empty
-        [TestCase("25", ResultText + "25!")]
-        [TestCase(new object[] { }, ResultText + "System.Object[]!")]
-        public void Method_Enrich_ReturnsFormattedText(object value, string expectedResult)
+        [TestCase(null, NewYearResult + "!")]  // Value is empty
+        [TestCase("", NewYearResult + "!")]    // Value is empty
+        [TestCase("25", NewYearResult + "25!")]
+        [TestCase(new object[] { }, NewYearResult + "System.Object[]!")]
+        public void Method_Enrich_ForNewYearMode_ReturnsFormattedText(object value, string expectedResult)
         {
             // Arrange
             var service = new RichTextService();
 
             // Act
-            var actualResult = service.Enrich(value);
+            var actualResult = service.Enrich(value, DisplayModeEnums.WelcomeNewYear);
+
+            // Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(null, StandardResult + "")]  // Value is empty
+        [TestCase("", StandardResult + "")]    // Value is empty
+        [TestCase("25", StandardResult + "25")]
+        [TestCase(new object[] { }, StandardResult + "System.Object[]")]
+        public void Method_Enrich_ForStandardMode_ReturnsFormattedText(object value, string expectedResult)
+        {
+            // Arrange
+            var service = new RichTextService();
+
+            // Act
+            var actualResult = service.Enrich(value, DisplayModeEnums.Standard);
 
             // Assert
             Assert.That(actualResult, Is.EqualTo(expectedResult));
