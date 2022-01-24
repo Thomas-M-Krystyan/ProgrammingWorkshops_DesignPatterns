c94bc64d63f563e79f0575bff02f488aca937db1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-01-25 00:16:29 +0100

    Init Command project

---
+++ Command/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Command.Controllers
+{
+    /// <summary>
+    /// The main MVC Controller to display the Home page.
+    /// </summary>
+    /// <seealso cref="Controller" />
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this._logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
