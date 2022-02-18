634d60e9dd8ca7002754949dce1e706ff64517c5
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-02-19 00:08:00 +0100

    Rename the class

---
+++ Command_Service/Commands/Implementations/ChangeTextBackgroundCommand.cs
@@ -0,0 +1,31 @@
+﻿using Command_Service.Commands.Interfaces;
+using Command_Service.Services.TextService.Interfaces;
+using System;
+
+namespace Command_Service.Commands.Implementations
+{
+    /// <summary>
+    /// Command changing background of the text.
+    /// </summary>
+    /// <summary>
+    /// <seealso cref="ICommand{T}"/>
+    public sealed class ChangeTextBackgroundCommand<T> : ICommand<T> where T : struct, Enum
+    {
+        private readonly ITextService _textService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangeTextBackgroundCommand{T}"/> class.
+        /// </summary>
+        /// <param name="textService">The text service.</param>
+        public ChangeTextBackgroundCommand(ITextService textService)
+        {
+            this._textService = textService;
+        }
+
+        /// <inheritdoc />
+        public string Execute(T color)
+        {
+            return this._textService.ChangeBackground(color.ToString().ToLower());
+        }
+    }
+}
