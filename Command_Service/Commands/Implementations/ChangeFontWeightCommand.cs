8ba2db063765866532a3e35ed7f7dba25612f9c9
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-02-19 00:08:27 +0100

    Implement font weight command

--- Command_Service/Commands/Implementations/ChangeFontWeightCommand.cs
+++ Command_Service/Commands/Implementations/ChangeFontWeightCommand.cs
@@ -1,10 +1,28 @@
-﻿namespace Command_Service.Commands.Implementations
+﻿using Command_Service.Commands.Interfaces;
+using Command_Service.Services.TextService.Interfaces;
+
+namespace Command_Service.Commands.Implementations
 {
     /// <summary>
     /// Command changing weight (bold effect) of the text.
     /// </summary>
-    public sealed class ChangeFontWeightCommand
+    public sealed class ChangeFontWeightCommand : ICommand<bool>
     {
-        // TODO: To be implemented
+        private readonly ITextService _textService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChangeFontWeightCommand"/> class.
+        /// </summary>
+        /// <param name="textService">The text service.</param>
+        public ChangeFontWeightCommand(ITextService textService)
+        {
+            this._textService = textService;
+        }
+
+        /// <inheritdoc />
+        public string Execute(bool isBold)
+        {
+            return this._textService.ChangeWeight(isBold);
+        }
     }
 }
