5c88fc995339157ec573fbb2713e0d84fad36134
Krystyan, Tomasz M <Tomasz.Krystyan@bakerhughes.com>
2022-05-11 14:50:50 +0200

    Provide 4 examples of formatted text in console

--- Command_Console/Program.cs
+++ Command_Console/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using Command_Service.Commands.Enums;
+using Command_Service.Commands.Implementations;
+using Command_Service.Commands.Interfaces;
 using Command_Service.Constants;
+using Command_Service.DomainModels;
+using Command_Service.Services.TextService.Implementations;
+using Command_Service.Services.TextService.Interfaces;
 
 namespace Command_Console
 {
@@ -7,8 +13,65 @@ namespace Command_Console
     {
         private static void Main()
         {
-            // TODO: Format the text using the same commands, but it should work for Console environment instead
-            Console.WriteLine(Utilities.Text);
+            ITextService consoleTextService = new ConsoleTextService();
+            ICommand consoleFontStyleCommand = new FontStyleCommand(consoleTextService);
+            
+            // -------------------------------------------------------------------------
+            // First example:
+            var styleParameters = new CommandParametersDto
+            {
+                ForegroundColor = ForegroundColorsEnum.Yellow,
+                BackgroundColor = BackgroundColorsEnum.Black,
+                IsFontBold = true,
+                Text = Utilities.Text
+            };
+
+            var formattedText = consoleFontStyleCommand.Execute(styleParameters);
+
+            Console.WriteLine(formattedText);
+
+            // -------------------------------------------------------------------------
+            // Second example:
+            styleParameters = new CommandParametersDto
+            {
+                ForegroundColor = ForegroundColorsEnum.Yellow,
+                BackgroundColor = BackgroundColorsEnum.Black,
+                IsFontBold = false,
+                Text = Utilities.Text
+            };
+
+            formattedText = consoleFontStyleCommand.Execute(styleParameters);
+
+            Console.WriteLine(formattedText);
+
+            // -------------------------------------------------------------------------
+            // Third example:
+            styleParameters = new CommandParametersDto
+            {
+                ForegroundColor = ForegroundColorsEnum.White,
+                BackgroundColor = BackgroundColorsEnum.Black,
+                IsFontBold = true,
+                Text = Utilities.Text
+            };
+
+            formattedText = consoleFontStyleCommand.Execute(styleParameters);
+
+            Console.WriteLine(formattedText);
+
+            // -------------------------------------------------------------------------
+            // Fourth example:
+            styleParameters = new CommandParametersDto
+            {
+                ForegroundColor = ForegroundColorsEnum.White,
+                BackgroundColor = BackgroundColorsEnum.Black,
+                IsFontBold = false,
+                Text = Utilities.Text
+            };
+
+            formattedText = consoleFontStyleCommand.Execute(styleParameters);
+
+            Console.WriteLine(formattedText);
+            // -------------------------------------------------------------------------
         }
     }
 }
