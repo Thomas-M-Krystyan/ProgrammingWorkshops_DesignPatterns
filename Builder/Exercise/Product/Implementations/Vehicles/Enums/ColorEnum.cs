ffdb33742f061af29a440447eb310261c35995eb
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 15:12:16 +0200

    Add vehicle enums

---
+++ Builder/Exercise/Product/Implementations/Vehicles/Enums/ColorEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Builder.Exercise.Product.Implementations.Vehicles.Enums
+{
+    public enum ColorEnum
+    {
+        White,
+        Red,
+        Blue,
+        Green,
+        Black
+    }
+}
---
+++ Builder/Exercise/Product/Implementations/Vehicles/Enums/ProductTypes/BikeTypesEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Builder.Exercise.Product.Implementations.Vehicles.Enums.ProductTypes
+{
+    public enum BikeTypesEnum
+    {
+        CityBike,
+        MountainBike,
+        RacingBike
+    }
+}
