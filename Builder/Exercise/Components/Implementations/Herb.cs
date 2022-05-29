912e367717fe461d2c1c23e026c42639da6a8631
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 15:35:21 +0200

    Add specific components

---
+++ Builder/Exercise/Components/Implementations/Herb.cs
@@ -0,0 +1,9 @@
+﻿namespace Builder.Exercise.Components.Implementations
+{
+    public sealed class Herb
+    {
+        public string Name { get; set; }
+
+        public ushort Grams { get; set; }
+    }
+}
---
+++ Builder/Exercise/Components/Implementations/PreparationMethod.cs
@@ -0,0 +1,11 @@
+﻿using Builder.Exercise.Products.Implementations.Meals.Enums;
+
+namespace Builder.Exercise.Components.Implementations
+{
+    public sealed class PreparationMethod
+    {
+        public PreparingMethodEnum Type { get; set; }
+
+        public ushort TemperatureInC { get; set; }
+    }
+}
