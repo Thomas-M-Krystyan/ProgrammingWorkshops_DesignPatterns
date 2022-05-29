0a4d54b7815cf887831097c7e3c5b4d6755a8a6c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 15:19:04 +0200

    Add base meal class

---
+++ Builder/Exercise/Product/Implementations/Meals/Models/Base/BaseMeal.cs
@@ -0,0 +1,13 @@
+﻿using Builder.Exercise.Product.Interfaces;
+
+namespace Builder.Exercise.Product.Implementations.Meals.Models.Base
+{
+    /// <summary>
+    /// Base class for meal products.
+    /// </summary>
+    /// <seealso cref="IProduct" />
+    public abstract class BaseMeal : IProduct
+    {
+        public string Name { get; set; }
+    }
+}
