12f994a6c1d3f4736dc85819e9393282eeee0ee1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 15:12:03 +0200

    Add vehicle models

---
+++ Builder/Exercise/Product/Implementations/Vehicles/BaseVehicle.cs
@@ -0,0 +1,22 @@
+﻿using Builder.Exercise.Product.Implementations.Vehicles.Enums;
+using Builder.Exercise.Product.Interfaces;
+
+namespace Builder.Exercise.Product.Implementations.Vehicles
+{
+    /// <summary>
+    /// Base class for vehicle products.
+    /// </summary>
+    /// <seealso cref="IAggregatedProduct" />
+    public abstract class BaseVehicle : IProduct
+    {
+        public string Name { get; set; }
+
+        public float WeightInKg { get; set; }
+
+        public byte Wheels { get; protected set; }
+
+        public byte Doors { get; protected set; }
+
+        public ColorEnum Color { get; set; }
+    }
+}
---
+++ Builder/Exercise/Product/Implementations/Vehicles/Bike.cs
@@ -0,0 +1,14 @@
+﻿namespace Builder.Exercise.Product.Implementations.Vehicles
+{
+    public sealed class Bike : BaseVehicle
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bike"/> class.
+        /// </summary>
+        public Bike()
+        {
+            this.Wheels = 2;
+            this.Doors = 0;
+        }
+    }
+}
