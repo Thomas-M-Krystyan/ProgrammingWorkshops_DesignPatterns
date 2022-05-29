b5be0c01aea7798f58a8f9ef1c64adfbb93f36cd
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:16:10 +0200

    Update builder interfaces

---
+++ Builder/Exercise/Builder/Interfaces/IBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Builder.Exercise.Products.Interfaces;
+using System;
+
+namespace Builder.Exercise.Builder.Interfaces
+{
+    public interface IBuilder<TProduct, TEnum>
+        where TProduct : IProduct
+        where TEnum : Enum
+    {
+        TProduct Build(TEnum type);
+
+        void ResetComponents();
+    }
+}
---
+++ Builder/Exercise/Builder/Interfaces/IFoodBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Builder.Exercise.Components.Implementations.Models;
+using Builder.Exercise.Components.Implementations.Models.Base;
+
+namespace Builder.Exercise.Builder.Interfaces
+{
+    /// <summary>
+    /// Provides methods suitable to create a food products.
+    /// </summary>
+    public interface IFoodBuilder
+    {
+        void InitializeProduct(string name, ushort weight);
+
+        void AddSpices(params Spice[] spices);
+
+        void AddHerbs(params Herb[] herbs);
+
+        void AddIngredients(params BaseIngredient[] ingredients);
+    }
+}
