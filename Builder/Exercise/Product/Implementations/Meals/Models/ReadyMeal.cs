f044743e2c2f68bf8fa2a43dce85838531bb3495
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 15:19:16 +0200

    Add ready meal model

---
+++ Builder/Exercise/Product/Implementations/Meals/Models/ReadyMeal.cs
@@ -0,0 +1,17 @@
+﻿using Builder.Exercise.Product.Implementations.Meals.Enums;
+using Builder.Exercise.Product.Implementations.Meals.Models.Base;
+using System.Collections.Generic;
+
+namespace Builder.Exercise.Product.Implementations.Meals.Models
+{
+    public sealed class ReadyMeal : BaseMeal
+    {
+        public ushort WeightInKg { get; set; }
+
+        public (ushort Min, ushort Max) CookingTimeInMinutes { get; set; }
+
+        public ICollection<(PreparingMethodEnum, ushort)> CookingTemperatureInC { get; set; }
+
+        public NutriScoreEnum NutriScore { get; set; }
+    }
+}
