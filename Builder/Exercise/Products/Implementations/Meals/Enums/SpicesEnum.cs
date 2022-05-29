fd0ea802599af7c4643efb80ec547839ef7b9f7c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:24:33 +0200

    Add spices enum

---
+++ Builder/Exercise/Products/Implementations/Meals/Enums/SpicesEnum.cs
@@ -0,0 +1,28 @@
+﻿namespace Builder.Exercise.Products.Implementations.Meals.Enums
+{
+    public enum SpicesEnum
+    {
+        // Salt
+        SeaSalt,
+        MountainSalt,
+
+        // Pepper
+        WhitePepper,
+        PinkPepper,
+        GreenPepper,
+        BlackPepper,
+        
+        // Paprika
+        SweetPaprika,
+        SpicyPaprika,
+        
+        // Granulated
+        GranulatedGarlic,
+        GranulatedOnion,
+        
+        // Others
+        Sesame,
+        Cinamon,
+        Ginger
+    }
+}
