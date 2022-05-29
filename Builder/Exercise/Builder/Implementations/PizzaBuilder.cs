6e90fc736548bf6a3f7b04ae87a8cbdb512a37c8
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:31:52 +0200

    Add unfinished PizzaBuilder

---
+++ Builder/Exercise/Builder/Implementations/PizzaBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Builder.Exercise.Builder.Interfaces;
+using Builder.Exercise.Components.Implementations.Models;
+using Builder.Exercise.Components.Implementations.Models.Base;
+using Builder.Exercise.Products.Implementations.Meals.Enums;
+using Builder.Exercise.Products.Implementations.Meals.Enums.ProductTypes;
+using Builder.Exercise.Products.Implementations.Meals.Models;
+using System;
+
+namespace Builder.Exercise.Builder.Implementations
+{
+    public sealed class PizzaBuilder : IBuilder<Pizza, PizzaTypesEnum>, IFoodBuilder
+    {
+        private readonly Pizza _pizza = new();
+
+        // -----------
+        // IBuilder<T>
+        // -----------
+
+        public Pizza Build(PizzaTypesEnum pizzaType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetComponents()
+        {
+            throw new NotImplementedException();
+        }
+
+        // ------------
+        // IFoodBuilder
+        // ------------
+
+        public void InitializeProduct(string name, ushort weightInGrams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSpices(params Spice[] spices)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddHerbs(params Herb[] herbs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddIngredients(params BaseIngredient[] ingredients)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
