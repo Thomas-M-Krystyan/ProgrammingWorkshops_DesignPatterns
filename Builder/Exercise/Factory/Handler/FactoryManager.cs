cd5b8041c436e6249b5bc7aea3b803630d31c4a8
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:29:24 +0200

    Add unfinished FactoryManager

---
+++ Builder/Exercise/Factory/Handler/FactoryManager.cs
@@ -0,0 +1,19 @@
+﻿using Builder.Exercise.Factory.Interfaces;
+using Builder.Exercise.Products.Interfaces;
+
+namespace Builder.Exercise.Factory.Handler
+{
+    /// <summary>
+    /// Handles which <see cref="IFactory"/> will be used and which products will be created.
+    /// </summary>
+    public sealed class FactoryManager  // NOTE: Singleton
+    {
+        /// <summary>
+        /// Creates a basic instance of the <see cref="IProduct"/>.
+        /// </summary>
+        public IProduct Create<T>() where T : IProduct, new()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
