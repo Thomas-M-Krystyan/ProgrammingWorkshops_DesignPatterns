b54e6fe20761823f35b59fbf280488aef96684c1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:32:46 +0200

    Add unfinished BuilderDirector

---
+++ Builder/Exercise/Builder/Handler/BuilderDirector.cs
@@ -0,0 +1,14 @@
+﻿using Builder.Exercise.Builder.Interfaces;
+using Builder.Exercise.Products.Interfaces;
+using System;
+
+namespace Builder.Exercise.Builder.Handler
+{
+    public class BuilderDirector  // NOTE: Singleton
+    {
+        public IProduct Build(Enum type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
