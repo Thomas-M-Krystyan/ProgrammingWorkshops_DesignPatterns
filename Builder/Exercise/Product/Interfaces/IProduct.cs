41a8aab2f3007a85954f7cfaba91ef71d69813bf
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 15:10:39 +0200

    Add Product interface

---
+++ Builder/Exercise/Product/Interfaces/IProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace Builder.Exercise.Product.Interfaces
+{
+    /// <summary>
+    /// This interface exposes only the name of the product. Particular components
+    /// needs to be declared explicit on the product level (e.g., as properties).
+    /// </summary>
+    public interface IProduct
+    {
+        string Name { get; set; }
+    }
+}
