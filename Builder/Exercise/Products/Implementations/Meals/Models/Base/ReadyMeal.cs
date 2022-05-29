a6e38a88691cd342eb2e346079201e38dfe60488
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:21:13 +0200

    Move ReadyMeal model to base folder

---
+++ Builder/Exercise/Products/Implementations/Meals/Models/Base/ReadyMeal.cs
@@ -0,0 +1,14 @@
+﻿using Builder.Exercise.Products.Implementations.Meals.Enums;
+using System.Collections.Generic;
+
+namespace Builder.Exercise.Products.Implementations.Meals.Models.Base
+{
+    public abstract class ReadyMeal : CustomMeal
+    {
+        public ushort WeightInGrams { get; set; }
+
+        public List<PreparationMethod> PreparationMethods { get; } = new List<PreparationMethod>();
+
+        public NutriScoreEnum NutriScore { get; set; }
+    }
+}
