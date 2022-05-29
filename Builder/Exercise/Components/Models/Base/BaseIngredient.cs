1d2f41222766e749039577537527b91e613ca5bf
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:25:41 +0200

    Introduce base ingredient model

---
+++ Builder/Exercise/Components/Models/Base/BaseIngredient.cs
@@ -0,0 +1,12 @@
+﻿using Builder.Exercise.Components.Interfaces;
+using Builder.Exercise.Products.Implementations.Meals.Enums;
+
+namespace Builder.Exercise.Components.Implementations.Models.Base
+{
+    public abstract class BaseIngredient : IComponent
+    {
+        public IngredientsEnum Type { get; set; }
+
+        public abstract ushort Amount { get; set; }
+    }
+}
