56f88c6902cb3e4b4e4adfb6057ea3df8ab11114
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:24:52 +0200

    Add ingredient enum

---
+++ Builder/Exercise/Products/Implementations/Meals/Enums/IngredientsEnum.cs
@@ -0,0 +1,57 @@
+﻿namespace Builder.Exercise.Products.Implementations.Meals.Enums
+{
+    public enum IngredientsEnum
+    {
+        // Cheese
+        GoudaCheese,
+        EdamerCheese,
+        MascarponeCheese,
+        ParmesanCheese,
+        MozarellaCheese,
+
+        // Meat
+        Sausage,
+        Pepperoni,
+        Prosciutto,
+        PorkHam,
+        BeefHam,
+        Chicken,
+
+        // Veggies
+        Potatoes,
+        SweetPotatoes,
+        Carrot,
+        Paprika,
+        Cucumber,
+        Tomatoes,
+        Garlic,
+        Onion,
+        Cabbage,
+
+        // Fruits
+        Apple,
+        Pear,
+        Grapes,
+        Raspberries,
+        StrawBerries,
+        BlackBerries,
+        Pineapple,
+        Banana,
+        Olives,
+
+        // Sauces
+        TomatoSauce,
+        BbqSauce,
+        CheeseSauce,
+        GarlicSauce,
+        SweetSourCauce,
+        HollandaiseSauce,
+
+        // Fats
+        Butter,
+        SunflowerOil,
+        PeanutOil,
+        SaladOil,
+        OliveOil
+    }
+}
