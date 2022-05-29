ee1ef7d3758b19943f9f56ce80ef382ff30dc611
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:23:39 +0200

    Add CustomMeal base model

---
+++ Builder/Exercise/Products/Implementations/Meals/Models/Base/CustomMeal.cs
@@ -0,0 +1,11 @@
+﻿using Builder.Exercise.Components.Interfaces;
+using Builder.Exercise.Products.Interfaces;
+using System.Collections.Generic;
+
+namespace Builder.Exercise.Products.Implementations.Meals.Models.Base
+{
+    public abstract class CustomMeal : BaseMeal, IProductComponentized
+    {
+        public List<IComponent> Components { get; } = new List<IComponent>();
+    }
+}
