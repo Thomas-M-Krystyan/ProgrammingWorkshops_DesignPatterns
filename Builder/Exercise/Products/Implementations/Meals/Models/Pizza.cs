e3074f8427ad61471ebd3a90dc60d26e9e1ef3d6
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:22:17 +0200

    Add Pizza class with another nested class

---
+++ Builder/Exercise/Products/Implementations/Meals/Models/Pizza.cs
@@ -0,0 +1,25 @@
+﻿using Builder.Exercise.Products.Implementations.Meals.Enums;
+using Builder.Exercise.Products.Implementations.Meals.Models.Base;
+
+namespace Builder.Exercise.Products.Implementations.Meals.Models
+{
+    public class Pizza : ReadyMeal
+    {
+    }
+
+    public sealed class OetkerRistorantePollo : Pizza
+    {
+        public OetkerRistorantePollo()
+        {
+            this.Name = "Dr. Oetker Ristorante Pollo Pizza";
+            this.WeightInGrams = 355;
+            this.PreparationMethods.AddRange(new []
+            {
+                new PreparationMethod { Type = PreparingMethodEnum.MicrovaweProcessing,  CookingTimeInMinutes = 7 },
+                new PreparationMethod { Type = PreparingMethodEnum.CookingInOvenWithFan, CookingTimeInMinutes = 11, TemperatureInC = 200 },
+                new PreparationMethod { Type = PreparingMethodEnum.CookingInOven,        CookingTimeInMinutes = 14, TemperatureInC = 220 }
+            });
+            this.NutriScore = NutriScoreEnum.C;
+        }
+    }
+}
