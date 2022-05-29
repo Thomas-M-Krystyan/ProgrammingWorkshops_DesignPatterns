2721f8e58c5119bdbd7511fa73fe4b54b4ecce1a
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-05-29 20:25:24 +0200

    Split ingredients into Solid and Liquid

---
+++ Builder/Exercise/Components/Models/Implementations/IngredientLiquid.cs
@@ -0,0 +1,12 @@
+﻿using Builder.Exercise.Components.Implementations.Models.Base;
+
+namespace Builder.Exercise.Components.Implementations.Models
+{
+    public sealed class IngredientLiquid : BaseIngredient
+    {
+        /// <summary>
+        /// Milliliters.
+        /// </summary>
+        public override ushort Amount { get; set; }
+    }
+}
---
+++ Builder/Exercise/Components/Models/Implementations/IngredientSolid.cs
@@ -0,0 +1,12 @@
+﻿using Builder.Exercise.Components.Implementations.Models.Base;
+
+namespace Builder.Exercise.Components.Implementations.Models
+{
+    public sealed class IngredientSolid : BaseIngredient
+    {
+        /// <summary>
+        /// Grams.
+        /// </summary>
+        public override ushort Amount { get; set; }
+    }
+}
